5665d146116d55cfdfa877332cc55956274edf2f
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-09-20 12:39:43 -0700

    questions added for each employee type; stored in corresponding object

--- Develop/app.js
+++ Develop/app.js
@@ -8,47 +8,141 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const {render, renderManager} = require("./lib/htmlRenderer");
+const {render, renderManager, renderEngineer, renderIntern} = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+// Inputs for Manager, Engineer, & Intern
 inquirer.prompt(
     [
         {
-            type: "input",
-            message: "please give manager name:",
-            name: "managerName",
-            validate: function(answer) {
-                if (answer !== "") { //managerName
-                    return true;
-                }
-                return "Please enter at least character"
-            }
-        },
-        {
-            type: "input",
-            message: "please give manager id:",
-            name: "managerId"
-        },
-        {
-            type: "input",
-            message: "please give manager email:",
-            name: "managerEmail"
-        },
-        {
-            type: "input",
-            message: "please give manager office number:",
-            name: "managerOfficeNumber"
+            type: "list",
+            message: "Please select employee type: ",
+            choices: [
+                "manager", "engineer", "intern"
+            ],
+            name: "employeeType"
         }
     ]
 ).then(function(answers) {
-    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
-    console.log(manager)
-    const managerHtml = renderManager(manager);
-    console.log(managerHtml);
-})
+    // if statment for employee type: manager, engineer, or intern
+    if (answers.employeeType === "manager") {
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    message: "please give manager name:",
+                    name: "managerName",
+                    validate: function(answer) {
+                        if (answer !== "") { //managerName
+                            return true;
+                        }
+                        return "Please enter at least character"
+                    }
+                },
+                {
+                    type: "input",
+                    message: "please give manager id:",
+                    name: "managerId"
+                },
+                {
+                    type: "input",
+                    message: "please give manager email:",
+                    name: "managerEmail"
+                },
+                {
+                    type: "input",
+                    message: "please give manager office number:",
+                    name: "managerOfficeNumber"
+                }
+            ]
+        ).then(function(resp) {
+            const manager = resp;
+            console.log(manager);
+        })
+    }
+    else if (answers.employeeType === "engineer"){
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    message: "please give engineer's name:",
+                    name: "engineerName",
+                    validate: function(answer) {
+                        if (answer !== "") { //engineerName
+                            return true;
+                        }
+                        return "Please enter at least character"
+                    }
+                },
+                {
+                    type: "input",
+                    message: "please give engineer id:",
+                    name: "engineerId"
+                },
+                {
+                    type: "input",
+                    message: "please give engineer email:",
+                    name: "engineerEmail"
+                },
+                {
+                    type: "input",
+                    message: "please give engineer's Github':",
+                    name: "engineerGithub"
+                }
+            ]
+        ).then(function(resp) {
+            const engineer = resp;
+            console.log(engineer);
+        })
+    }
+    else if (answers.employeeType === "intern"){
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    message: "please give intern's name:",
+                    name: "internName",
+                    validate: function(answer) {
+                        if (answer !== "") { //internName
+                            return true;
+                        }
+                        return "Please enter at least character"
+                    }
+                },
+                {
+                    type: "input",
+                    message: "please give intern id:",
+                    name: "internId"
+                },
+                {
+                    type: "input",
+                    message: "please give intern email:",
+                    name: "internEmail"
+                },
+                {
+                    type: "input",
+                    message: "please give intern's school':",
+                    name: "internSchool"
+                }
+            ]
+        ).then(function(resp) {
+            const intern = resp;
+            console.log(intern);
+        })
+    }
+
+
+    // const managerHtml = renderManager(manager); // creates the HTML for the manager
+    // const engineerHtml = renderEngineer(engineer); // creates the HTML for the engineer
+    // const internHtml = renderIntern(intern); // creates the HTML for the intern
+    // console.log(managerHtml, engineerHtml, internHtml);
+});
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,11 +1,10 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const employee = require("./Employee");
+const Employee = require("./Employee");
 
 class Manager extends Employee { // manager has employee properties & methods
-    constructor(name, id, email) {
-        super(name, id, email); // this creates new Employee(name, id, email)
+    constructor(name, id , email, officeNumber) {       
+        super(name, id, email); // this creates new Employee(name, id, eamil)
         this.officeNumber = officeNumber;
-    
     }
 
     getRole() {
